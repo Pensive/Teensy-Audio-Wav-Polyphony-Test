d4b30772ba8452e39a0729d6dd6e48851f18e311
Pensive <jon@jonnypayne.com>
2015-06-07 22:26:28 +0100

    First Issue
    First issue

---
+++ Release/usb_serial.c.d
@@ -0,0 +1,9 @@
+C:\Users\Jon\ Payne\AppData\Local\VMicro\Arduino\Builds\wavpolytest\teensy31\usb_serial.c.o: \
+ C:\Program\ Files\ (x86)\Arduino\hardware\teensy\cores\teensy3\usb_serial.c \
+ C:\Program\ Files\ (x86)\Arduino\hardware\teensy\cores\teensy3\usb_dev.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\teensy\cores\teensy3\usb_desc.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\teensy\cores\teensy3\usb_mem.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\teensy\cores\teensy3\usb_serial.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\teensy\cores\teensy3\core_pins.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\teensy\cores\teensy3\kinetis.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\teensy\cores\teensy3\pins_arduino.h
---
+++ Release/yield.cpp.d
@@ -0,0 +1,15 @@
+C:\Users\Jon\ Payne\AppData\Local\VMicro\Arduino\Builds\wavpolytest\teensy31\yield.cpp.o: \
+ C:\Program\ Files\ (x86)\Arduino\hardware\teensy\cores\teensy3\yield.cpp \
+ C:\Program\ Files\ (x86)\Arduino\hardware\teensy\cores\teensy3\core_pins.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\teensy\cores\teensy3\kinetis.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\teensy\cores\teensy3\pins_arduino.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\teensy\cores\teensy3\HardwareSerial.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\teensy\cores\teensy3\Stream.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\teensy\cores\teensy3\Print.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\teensy\cores\teensy3\core_id.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\teensy\cores\teensy3\WString.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\teensy\cores\teensy3\avr_functions.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\teensy\cores\teensy3\Printable.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\teensy\cores\teensy3\new.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\teensy\cores\teensy3\usb_serial.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\teensy\cores\teensy3\usb_seremu.h
